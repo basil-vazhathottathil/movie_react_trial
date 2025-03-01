4f2906a2ed37886e71d6593c3c9b93adb0eef227
Basil Vazhathottathil <basilveldose@gmail.com>
2025-03-01 11:59:54 +0530

    created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import Home from './pages/Home';
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Favorites from './pages/Favorites';
+import NavBar from './components/NavBar';
 
 function App() {
   const movieno=1;
   return (
-    <main className='main-content'>
+    <div>
+      <NavBar/>
+      <main className='main-content'>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/favorites' element={<Favorites />} />
       </Routes>
     </main>
+    </div>
   )
 }
 export default App
