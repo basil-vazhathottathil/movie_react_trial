a31ca8163ca2df267d34bcd1c66b728f72a46b03
Basil Vazhathottathil <basilveldose@gmail.com>
2025-03-01 11:34:04 +0530

    page routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import MovieCard from './components/MovieCard'
 import Home from './pages/Home';
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Favorites from './pages/Favorites';
 
 function App() {
   const movieno=1;
   return (
-    <>
-      <Home />
-    </>
+    <main className='main-content'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/favorites' element={<Favorites />} />
+      </Routes>
+    </main>
   )
 }
 export default App
---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,11 @@
+function Favorites(){
+
+    return (
+        <div className="favorites-empty">
+            <h3>No favorites yet</h3>
+            <p>add movies to favorites....</p>
+        </div>
+    )
+}
+
+export default Favorites;  
